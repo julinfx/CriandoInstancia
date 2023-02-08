18d5eec58f24f8a986c8422419b865cc929d9efe
Julio Cezar <jb.cezar18@gmail.com>
2023-02-08 19:28:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#Criando instancias usando a função lambda no AWS
+
+'''
+Script Json para criar a chave de permissão IAM Role
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Action": [
+        "ec2:RunInstances"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+
+'''
+
+import os
+import boto3
+
+AMI = os.environ['AMI']
+INSTANCE_TYPE = os.environ['INSTANCE_TYPE']
+KEY_NAME = os.environ['KEY_NAME']
+SUBNET_ID = os.environ['SUBNET_ID']
+
+ec2 = boto3.resource('ec2')
+
+
+def lambda_handler(event, context):
+
+    instance = ec2.create_instances(
+        ImageId=AMI,
+        InstanceType=INSTANCE_TYPE,
+        KeyName=KEY_NAME,
+        SubnetId=SUBNET_ID,
+        MaxCount=1,
+        MinCount=1
+    )
+
+    print("New instance created:", instance[0].id)
